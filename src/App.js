92f15803e6383829f668a27a3cb00f1add8c6a01
Oyedolacode <segunzyshegsy12@gmail.com>
2022-04-27 14:09:07 +0100

    api fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { useEffect } from 'react';
 import './App.css';
+import axios  from 'axios';
 
 function App() {
+  const dictionaryApi = async() => {
+    try {
+      const data = await axios.get(
+        "https://api.dictionaryapi.dev/api/v2/entries/en/plane"
+      );
+    } catch (error){
+    console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    dictionaryApi();
+
+  }, [])
+
+
   return <div className='App'>Hello World</div>;
     
 }
