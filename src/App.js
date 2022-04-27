aab3c622e07bf4e009e56cf9d7fdf91d474d12d4
Oyedolacode <segunzyshegsy12@gmail.com>
2022-04-27 18:09:07 +0100

    icon fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import axios  from 'axios';
-import { Container } from "@material-ui/core"
-import Header from './components/Header/Header';
-
+import { Container, Switch, withStyles } from "@material-ui/core";
+import { grey } from "@material-ui/core/colors";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import Definitions from "./components/Definitions/Definitions";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
   const [word, setWord] = useState("");
@@ -11,40 +13,84 @@ function App() {
   const [category, setCategory] = useState("en");
   const [LightTheme, setLightTheme] = useState(false);
 
-  const dictionaryApi = async() => {
+  const dictionaryApi = async () => {
     try {
       const data = await axios.get(
         `https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`
       );
       setMeanings(data.data);
-    } catch (error){
-    console.log(error);
+    } catch (error) {
+      console.log(error);
     }
   };
 
-  console.log(meanings)
+  console.log(meanings);
 
   useEffect(() => {
     dictionaryApi();
+    // eslint-disable-next-line
+  }, [word, category]);
 
-  }, [word,category])
-
+  const PurpleSwitch = withStyles({
+    switchBase: {
+      color: grey[50],
+      "&$checked": {
+        color: grey[900],
+      },
+      "&$checked + $track": {
+        backgroundColor: grey[500],
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
 
   return (
-    <div className='App' 
-    style={{height:'100vh', backgroundColor: '#456c62', color: 'white'}}
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+        backgroundColor: LightTheme ? "#fff" : "#282c34",
+        color: LightTheme ? "black" : "white",
+        transition: "all 0.5s linear",
+      }}
     >
-   <Container 
-   mdWidth="md"
-   style={{ display: 'flex', flexDirection: 'column', height:'100vh' }}
-   >
-    <Header 
-    category={category} 
-    setCategory={setCategory}
-    word={word}
-    setWord={setWord}
-    />
-   </Container>
+      <Container
+        maxWidth="md"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <div
+          style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}
+        >
+          <span>{LightTheme ? "Dark" : "Light"} Mode</span>
+          <PurpleSwitch
+            checked={LightTheme}
+            onChange={() => setLightTheme(!LightTheme)}
+          />
+        </div>
+        <Header
+          setWord={setWord}
+          category={category}
+          setCategory={setCategory}
+          word={word}
+          setMeanings={setMeanings}
+          LightTheme={LightTheme}
+        />
+        {meanings && (
+          <Definitions
+            meanings={meanings}
+            word={word}
+            LightTheme={LightTheme}
+            category={category}
+          />
+        )}
+      </Container>
+      <Footer />
     </div>
   );
 }
